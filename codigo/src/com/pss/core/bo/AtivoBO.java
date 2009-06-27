31a5a00ade89b7113a463d74741c44820c8b06a7
Gustavo Soares <gustavosoares@gmail.com>
2009-06-27 16:44:26 -0300

    notificacao de edicao apenas se monitoracao e seguranca

--- codigo/src/com/pss/core/bo/AtivoBO.java
+++ codigo/src/com/pss/core/bo/AtivoBO.java
@@ -86,7 +86,8 @@ public class AtivoBO implements AtivoRepository {
 		
 		//possui monitoracao?
 		boolean monitoracao = FacadeUtil.featureHabilitada("monitoracao");
-		if (monitoracao) {
+		boolean seguranca = FacadeUtil.featureHabilitada("seguranca");
+		if (monitoracao && seguranca) {
 			//notificar observadores
 			com.pss.features.monitoracao.agente1.bo.Agente1BO agenteBO = com.pss.features.monitoracao.agente1.bo.Agente1BO.getInstance();
 			agenteBO.notificarObservadores(ativo);
